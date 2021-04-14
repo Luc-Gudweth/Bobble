81b867efdbce5673f5fb4135fcff7d0193d54a4d
Luc-Gudweth <75072325+Luc-Gudweth@users.noreply.github.com>
2021-04-14 23:06:51 +0200

    Add files via upload

---
+++ Bobble Luc Gudweth.py
@@ -0,0 +1,115 @@
+import pygame
+import os
+import random
+
+
+
+time = 0
+draw = True
+button = False
+
+
+class Settings:
+    window_width = 1000
+    window_height = 800
+    window_border = 75
+    window_border2 = 0
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    image_path = os.path.join(file_path, "images")
+   
+    
+class Background(object):
+    def __init__(self, filename):
+        self.image = pygame.image.load(os.path.join(Settings.image_path, filename))
+        self.image = pygame.transform.scale(self.image, (Settings.window_width, Settings.window_height)).convert()
+        self.rect = self.image.get_rect()
+        
+    def draw(self, screen):
+        screen.blit(self.image, self.rect) 
+
+
+    
+class Bubble(pygame.sprite.Sprite):
+    def __init__(self, filename):
+        super().__init__()
+        bitmap = pygame.image.load(os.path.join(Settings.image_path, filename))
+        self.image = bitmap.convert_alpha()
+        self.image = pygame.transform.scale(self.image, (75, 75)).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.centerx = (Settings.window_width - 75) // 2
+        self.rect.bottom = Settings.window_height - Settings.window_border2 -10
+        self.random1 = 0
+        self.random2 = 0
+        
+        
+
+        
+
+    def update(self):
+        newrect = self.rect.move(self.random1, self.random2)
+        if newrect.top <= Settings.window_border2:
+             self.move_stop()
+        if newrect.bottom >= Settings.window_height - Settings.window_border2:
+             self.move_stop()
+        if newrect.left <= Settings.window_border2:
+            self.move_stop()
+        if newrect.right >= Settings.window_width - Settings.window_border2:
+            self.move_stop()
+        self.rect.move_ip(self.random1, self.random2)
+
+
+    def move_random(self):
+        self.rect.centerx = random.randint(75, 825)
+        self.random1 = self.rect.centerx
+        self.rect.bottom = random.randint(75, 625)
+        self.random2 = self.rect.bottom
+        print(self.random1, self.random2)
+
+    def move_stop(self):
+        self.random1 = 0
+        self.random2 = 0
+
+
+    
+
+if __name__ == '__main__':
+    pygame.init()
+    
+    screen = pygame.display.set_mode((Settings.window_width, Settings.window_height))
+    clock = pygame.time.Clock()
+    all_sprites = pygame.sprite.Group()
+    
+
+    
+    background = Background("background2.png")
+    bubble = Bubble("bubble.png")
+    all_sprites.add(bubble)
+    
+
+    
+    running = True
+    while running:
+        clock.tick(60)
+        x, y = pygame.mouse.get_pos()
+        time = time +1
+        if time == 200:
+            time = 0
+        elif time == 199:
+                bubble.move_random()
+                draw = True
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if y >= bubble.rect.bottom -45  and y <= bubble.rect.bottom + 25 and x >= bubble.rect.centerx -45 and x <= bubble.rect.centerx + 25:
+                        draw = False
+
+        bubble.update()
+        background.draw(screen)
+        if draw == True:
+            all_sprites.draw(screen)
+        pygame.display.flip()
+
+
+    pygame.quit()
